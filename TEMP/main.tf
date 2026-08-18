1dc9bff6d4f4243fcc4ff47a0f68731e884bc116
ys-o <77onizuka77@gmail.com>
2026-08-18 17:39:41 +0900

    20260818：IAM/AMI/AP

---
+++ TEMP/main.tf
@@ -0,0 +1,29 @@
+# ---------------------------------------------
+# Terraform configuration
+# ---------------------------------------------
+terraform {
+  required_version = ">=0.13"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# ---------------------------------------------
+# Provider
+# ---------------------------------------------
+provider "aws" {
+  profile = "terraform"
+  region  = "ap-northeast-1"
+}
+
+# ---------------------------------------------
+# Variables
+# ---------------------------------------------
+variable "message" {
+  type = string
+  default = "Hello World !"
+}
+
